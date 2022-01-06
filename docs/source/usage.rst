a7302f50ac75802b102803ca86fd5d703939ab4a
Andrea Zignoli <83694634+andreazignoli@users.noreply.github.com>
2022-01-06 17:03:00 +0100

    Adding to docs

--- docs/source/usage.rst
+++ docs/source/usage.rst
@@ -17,3 +17,7 @@ Functions
 To test the package you can use the ``utilities.PrintHello()`` function:
 
 .. autofunction:: utilities.PrintHello()
+
+The optimal filter adopted for the project ``utilities.optimal_filter(t, y, my_lambda)`` function:
+
+.. autofunction:: utilities.optimal_filter(t, y, my_lambda)
--- pyoxynet/utilities.py
+++ pyoxynet/utilities.py
@@ -20,3 +20,59 @@ def PrintHello(hello='hello'):
     print(hello)
     
     return
+
+def optimal_filter(t, y, my_lambda):
+    """A bad ass optimisation filter
+    
+    Parameters: 
+        t : array
+            Independent coord array
+        y : array
+            Dependent coord array
+        my_lambda : float
+            Smoothing factor
+
+    Returns:
+        array
+            Filtered variable
+    
+    """
+
+    import numpy as np
+    # be robust for non-monotonic x variables
+    for i in np.arange(1, len(t)-1):
+        if t[i+1] == t[i]:
+            t[i+1] = t[i] + 0.1
+
+    h  = 0.5 * np.concatenate([[t[1]-t[0]], t[2:] - t[0:-2], [t[-1] - t[-2]]])
+    # Robustness
+    m = np.median(h[h > 0])
+    # Assign the median to the zero elements
+    h[h == 0] = m
+
+    dg = np.divide(my_lambda,h)
+    # symmetric tri-diagonal system
+    a = - dg[1:]
+    b = np.diff(t) + dg[0:-1] + dg[1:]
+    u = np.diff(y)
+    # Solution of the system
+    n = len(u)
+
+    for j in np.arange(0, n - 1):
+        mu = a[j] / b[j]
+        b[j + 1] = b[j + 1] - mu * a[j]
+        u[j + 1] = u[j + 1] - mu * u[j]
+
+    u[n-1] = u[n-1] / b[n-1]
+
+    for j in np.arange(n-2, -1, -1):
+        u[j] = (u[j] - a[j] * u[j + 1]) / b[j]
+
+    # Retrieving solution
+    x = np.empty([len(y), ])
+    x[0] = y[0] + my_lambda * u[0] / h[0]
+
+    for i in np.arange(0, n):
+        x[i + 1] = x[i] + (t[i + 1] - t[i]) * u[i]
+
+    return x
