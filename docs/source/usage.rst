ab9c326bd6b1ac50acc83699687a90603bee0a83
Andrea Zignoli <83694634+andreazignoli@users.noreply.github.com>
2022-01-06 16:42:52 +0100

    Getting closer

--- docs/source/usage.rst
+++ docs/source/usage.rst
@@ -11,25 +11,6 @@ To use pyoxynet, first install it using pip:
    (.venv) $ pip install pyoxynet
 
 
-PrintHello
-----------------
-
-To test the package you can use the ``pyoxynet.PrintHello()`` function:
-
-.. py:function:: pyoxynet.PrintHello()
-
-    Parameters
-    ----------
-    nothing : str
-        Nothing at all
-    nothing_at_all : bool, optional
-        A flag not used to do anything (default is False)
-
-    Returns
-    -------
-    str
-        A str saying Hello
-
 To test the package you can use the ``utilities.PrintHello()`` function:
 
 .. autofunction:: utilities.PrintHello()
--- pyoxynet/utilities.py
+++ pyoxynet/utilities.py
@@ -1,18 +1,29 @@
 def PrintHello():
-    """A simple function that says hello...
-    
-    Parameters
-    ----------
-    nothing : str
-        Nothing at all
-    nothing_at_all : bool, optional
-        A flag not used to do anything (default is False)
+    """This function does something.
 
-    Returns
-    -------
-    str
-        A str saying Hello
-    
+    Args:
+       name (str):  The name to use.
+
+    Kwargs:
+       state (bool): Current state to be in.
+
+    Returns:
+       int.  The return code::
+
+          0 -- Success!
+          1 -- No good.
+          2 -- Try again.
+
+    Raises:
+       AttributeError, KeyError
+
+    A really great idea.  A way you might use me is
+
+    >>> print public_fn_with_googley_docstring(name='foo', state=None)
+    0
+
+    BTW, this always returns 0.  **NEVER** use with :class:`MyPublicClass`.
+
     """
     
     print("Hello from the Oxynet Python package")
