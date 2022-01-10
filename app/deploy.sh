48d6b47936b94a9a80a525a720078a605f97f6cf
Andrea Zignoli <83694634+andreazignoli@users.noreply.github.com>
2022-01-10 13:41:07 +0100

    Docker creation to deploy on AWS

--- app/deploy.sh
+++ app/deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+docker build -t flask-pyoxynet .
+docker run -p 9099:9099 flask-pyoxynet
 aws lightsail create-container-service --service-name flask-service --power small --scale 1
 aws lightsail push-container-image --service-name flask-service --label flask-pyoxynet --image flask-pyoxynet
 aws lightsail create-container-service-deployment --service-name flask-service --containers file://containers.json --public-endpoint file://public-endpoint.json
