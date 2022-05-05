130de459450d341ce3a4471e87951bc0d50151db
Andrea Zignoli <andrea.zignoli@unitn.it>
2022-05-05 14:24:25 +1000

    Testing

--- pyoxynet/debugging.py
+++ pyoxynet/debugging.py
@@ -2,11 +2,11 @@ import pickle
 
 from pyoxynet import *
 
-generator = load_tf_generator()
+# generator = load_tf_generator()
 
 real_df, data_dict_real = draw_real_test()
 
-df, data_dict_fake = generate_CPET(generator, plot=False, fitness_group=2)
+# df, data_dict_fake = generate_CPET(generator, plot=False, fitness_group=2)
 test_pyoxynet(input_df=real_df)
 
 here =0
