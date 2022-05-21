3e7554fa61a4f072b62fa2be50726f9f3c62c97f
Andrea Zignoli <andrea.zignoli@unitn.it>
2022-05-22 07:45:15 +1000

    Tests

--- pyoxynet/debugging.py
+++ pyoxynet/debugging.py
@@ -9,9 +9,11 @@ generator = load_tf_generator()
 
 # real_df, data_dict_real = draw_real_test()
 
-for i_ in np.arange(10):
+for i_ in np.arange(20):
     df, data_dict_fake = generate_CPET(generator)
     df.to_csv('/Users/andreazignoli/oxynet-interpreter-tf2/generated/gen_' + str(i_) + '.csv')
     # test_pyoxynet(input_df=real_df)
+    # plt.plot(df.VO2_I)
+    # here=0
 
 here =0
