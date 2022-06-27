3a8c497402581111e482b437e81c761f16f08dba
Andrea Zignoli <andrea.zignoli@unitn.it>
2022-06-27 14:02:20 +1000

    Uploading v10

--- pyoxynet/setup.py
+++ pyoxynet/setup.py
@@ -10,7 +10,7 @@ long_description = (this_directory/"README.md").read_text()
 
 setuptools.setup(
     name="pyoxynet",
-    version="0.0.9.9",
+    version="0.0.10",
     author="Andrea Zignoli",
     author_email="andrea.zignoli@unitn.it",
     description="Python package of the Oxynet project",
