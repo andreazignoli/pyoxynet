ea743c73fe0bd2b89ee283b1cd37b0046fdc30cc
Andrea Zignoli <andrea.zignoli@unitn.it>
2022-10-31 17:29:40 +1000

    Uploading models with weights

--- pyoxynet/pyoxynet/model.py
+++ pyoxynet/pyoxynet/model.py
@@ -1,5 +1,73 @@
 import tensorflow as tf
 
+class Generator(tf.keras.Model):
+    def __init__(self, n_input=7, n_past_points=40, n_labels=3, data_noise_dim=50):
+
+        super(Generator, self).__init__()
+        # DATA
+        self.d1 = tf.keras.layers.Dense(n_past_points * n_input, use_bias=False, input_shape=(data_noise_dim,))
+        self.bn1 = tf.keras.layers.BatchNormalization()
+        self.lRe1 = tf.keras.layers.LeakyReLU()
+
+        self.conv1 = tf.keras.layers.Conv1DTranspose(32, 5, strides=1, padding='same', use_bias=False)
+        self.bn2 = tf.keras.layers.BatchNormalization()
+        self.lRe2 = tf.keras.layers.LeakyReLU()
+
+        # LABEL
+        self.d12 = tf.keras.layers.Dense(n_past_points * n_labels, use_bias=False, input_shape=(3,))
+        self.bn12 = tf.keras.layers.BatchNormalization()
+        self.lRe12 = tf.keras.layers.LeakyReLU()
+
+        self.conv12 = tf.keras.layers.Conv1DTranspose(32, 5, strides=1, padding='same', use_bias=False)
+        self.bn22 = tf.keras.layers.BatchNormalization()
+        self.lRe22 = tf.keras.layers.LeakyReLU()
+
+        # DATA + LABEL
+        self.conv2 = tf.keras.layers.Conv1DTranspose(32, 5, strides=2, padding='same', use_bias=False)
+        self.bn3 = tf.keras.layers.BatchNormalization()
+        self.lRe3 = tf.keras.layers.LeakyReLU()
+
+        self.conv3 = tf.keras.layers.Conv1D(n_input, 5, strides=2, padding='same', use_bias=False, activation='tanh')
+        self.bn32 = tf.keras.layers.BatchNormalization()
+        self.lRe32 = tf.keras.layers.LeakyReLU()
+
+    def call(self, inputs, training=None):
+        # data
+        inputs_1 = tf.slice(inputs, [0, 0], [-1, 50])
+        # label
+        inputs_2 = tf.slice(inputs, [0, 50], [-1, 3])
+
+        # INPUT
+        x = self.d1(inputs_1)
+        x = self.bn1(x, training=training)
+        x = self.lRe1(x, training=training)
+        x = tf.reshape(x, [-1, 40, 7])
+
+        # LABELS
+        y = self.d12(inputs_2)
+        y = self.bn2(y, training=training)
+        y = self.lRe2(y, training=training)
+        y = tf.reshape(y, [-1, 40, 3])
+
+        # LABELS + INPUT
+        x = tf.concat([x, y], axis=2)
+        x = self.conv12(x)
+        x = self.bn22(x, training=training)
+        x = self.lRe22(x, training=training)
+
+        x = self.conv2(x)
+        x = self.bn3(x, training=training)
+        x = self.lRe3(x, training=training)
+
+        x = self.conv3(x)
+        x = self.bn32(x, training=training)
+        x = self.lRe32(x, training=training)
+
+        return x
+
+    def loadModel(self, model_path):
+        self.load_weights(model_path)
+
 class Model(tf.keras.Model):
 
     def __init__(self, n_classes, n_input):
@@ -40,19 +108,7 @@ class Model(tf.keras.Model):
         x = self.bn1(x, training=training)
         x = self.drop1(x, training=training)
 
-        # x = self.conv2(x)
-        # x = self.avg2(x)
-        # x = self.bn2(x, training=training)
         x = self.f2(x, training=training)
-        # x = self.drop2(x, training=training)
-
-        # x = self.d1(x)
-        # x = self.drop3(x, training=training)
-        # x = self.bn3(x, training=training)
-        #
-        # x = self.d2(x)
-        # x = self.drop4(x, training=training)
-        # x = self.bn4(x, training=training)
 
         x = self.d3(x)
         x = self.drop5(x, training=training)
@@ -61,5 +117,4 @@ class Model(tf.keras.Model):
         return self.d4(x)
 
     def loadModel(self, model_path):
-        # self.model = Model(n_classes, n_input)
         self.load_weights(model_path)
