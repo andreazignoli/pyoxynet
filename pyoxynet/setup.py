7d8a66acf6d945051274cd389e8c94357eb785e7
Andrea Zignoli <andrea.zignoli@unitn.it>
2022-12-13 09:10:16 +0100

    Uploaded 12.1 w/o matplotlib in setup and everywhere else

--- pyoxynet/setup.py
+++ pyoxynet/setup.py
@@ -10,7 +10,7 @@ long_description = (this_directory/"README.md").read_text()
 
 setuptools.setup(
     name="pyoxynet",
-    version="0.0.12.0",
+    version="0.0.12.1",
     author="Andrea Zignoli",
     author_email="andrea.zignoli@unitn.it",
     description="Python package of the Oxynet project",
@@ -18,7 +18,7 @@ setuptools.setup(
     long_description_content_type='text/markdown',
     packages=setuptools.find_packages(),
     install_requires=['importlib-resources', 'pandas', 
-    'uniplot', 'scipy', 'tensorflow', 'shap', 'matplotlib'],
+    'uniplot', 'scipy', 'tensorflow', 'shap'],
     classifiers=[
         "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
