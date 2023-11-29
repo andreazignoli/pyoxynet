b15956f63c2188734128e1a07abb4efc5c6ce12b
Andrea Zignoli <andrea.zignoli@unitn.it>
2023-11-29 14:42:47 +0100

    Ready to push 17.8

--- pyoxynet/setup.py
+++ pyoxynet/setup.py
@@ -10,7 +10,7 @@ long_description = (this_directory/"README.md").read_text()
 
 setuptools.setup(
     name="pyoxynet",
-    version="0.0.17.7",
+    version="0.0.17.8",
     author="Andrea Zignoli",
     author_email="andrea.zignoli@unitn.it",
     description="Python package of the Oxynet project",
